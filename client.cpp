3de180a54d887e26a63d371fe3c2e4d860b26aad
KChenDu <kelvenchendu@gmail.com>
2021-09-07 00:02:54 -0300

    Usage changed to only one argument(URL)

--- client.cpp
+++ client.cpp
@@ -5,7 +5,6 @@
 #include <sstream>
 #include <string>
 
-#define SERVER_PORT 80
 #define BUFSIZE 4096
 
 /* Requests a file from the server program `server.cpp`
@@ -13,13 +12,22 @@
  * argv[2]: file name to request
  */
 int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        std::cerr << "Usage: client host-name file-name" << std::endl;
-        exit(-1);
+    if (argc != 2) {
+        std::cerr << "Usage: client http://[host-name]:[port]/[file-name]" << std::endl;
+        return -1;
     }
 
-    char *hostname = argv[1];
-    struct hostent *host = gethostbyname(hostname);
+    std::string s = argv[1];
+    if (s.substr(0, 7) == "http://")
+        s.erase(0, 7);
+
+    int pos = s.find_first_of(':');
+    if (pos == std::string::npos) {
+        std::cerr << "Invalid URL" << std::endl << "Please enter http://[host-name]:[port]/[file-name]" << std::endl;
+        return -1;
+    }
+    std::string hostname = s.substr(0, pos);
+    struct hostent *host = gethostbyname(hostname.c_str());
     if (!host) {
         std::cerr << "gethostbyname failed to locate " << hostname << std::endl;
         exit(-1);
@@ -35,17 +43,18 @@ int main(int argc, char *argv[]) {
     memset(&channel, 0, sizeof(channel));
     channel.sin_family = AF_INET;
     memcpy(&channel.sin_addr.s_addr, host->h_addr, host->h_length);
-    channel.sin_port = htons(SERVER_PORT);
-
+    s.erase(0, pos + 1);
+    pos = s.find_first_of('/');
+    int port = std::stoi(s.substr(0, pos));
+    channel.sin_port = htons(port);
     if (connect(socket_fd, (struct sockaddr *) &channel, sizeof(channel)) < 0) {
         std::cerr << "connect failed" << std::endl;
         exit(-1);
     }
 
-    char *filename = argv[2];
+    std::string filename = s.substr(pos);
     std::stringstream ss;
-    ss << "GET " << filename << " HTTP/1.1\r\nHost: " << hostname << "\r\nAccept: text/html\r\n\r\n";
-    std::string request = ss.str();
+    std::string request = "GET " + filename + " HTTP/1.1\r\nHost: " + hostname + "\r\nAccept: text/html\r\n\r\n";
     write(socket_fd, request.c_str(), request.length());
     send(socket_fd, request.c_str(), request.length(), 0);
     int buffer[BUFSIZE];
--- readme.md
+++ readme.md
@@ -6,8 +6,8 @@ To build web-client, please tap:
 
 Here is the usage of web-client:
 
-`./web-client [host_name] [file_name]`
+`./web-client http://[host_name]:[port]/[file_name]`
 
 For example, you can have an HTTP GET by:
 
-`./web-client gaia.cs.umass.edu /wireshark-labs/HTTP-wireshark-file1.html`
+`./web-client gaia.cs.umass.edu:80/wireshark-labs/HTTP-wireshark-file1.html`
